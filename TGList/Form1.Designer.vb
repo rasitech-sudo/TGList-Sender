36ac3e888d52b79f173b5ea8e4fc14b255c17140
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-07-24 10:43:12 +0700

    Add files via upload
    Update V 3.0

--- TGList/Form1.Designer.vb
+++ TGList/Form1.Designer.vb
@@ -35,26 +35,36 @@ Partial Class Form1
         Me.Button4 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox4.SuspendLayout()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -72,7 +82,7 @@ Partial Class Form1
         Me.ListBox1.Location = New System.Drawing.Point(265, 14)
         Me.ListBox1.Name = "ListBox1"
         Me.ListBox1.ScrollAlwaysVisible = True
-        Me.ListBox1.Size = New System.Drawing.Size(174, 147)
+        Me.ListBox1.Size = New System.Drawing.Size(174, 160)
         Me.ListBox1.TabIndex = 1
         '
         'Button1
@@ -139,7 +149,6 @@ Partial Class Form1
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.Button5)
         Me.GroupBox1.Controls.Add(Me.TextBox1)
         Me.GroupBox1.Controls.Add(Me.ListBox1)
@@ -151,6 +160,24 @@ Partial Class Form1
         Me.GroupBox1.TabIndex = 9
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "List"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 27)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Total : "
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(224, 48)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(35, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "C"
+        Me.Button5.UseVisualStyleBackColor = True
         '
         'GroupBox2
         '
@@ -170,30 +197,47 @@ Partial Class Form1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Settings"
         '
-        'Label1
+        'Label7
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(6, 19)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 13)
-        Me.Label1.TabIndex = 7
-        Me.Label1.Text = "Interval :"
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(41, 175)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(225, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Message / Audio / Document / Photo / Video"
         '
-        'Button5
+        'TextBox5
         '
-        Me.Button5.Location = New System.Drawing.Point(224, 48)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(35, 23)
-        Me.Button5.TabIndex = 9
-        Me.Button5.Text = "C"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.TextBox5.Location = New System.Drawing.Point(9, 152)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(302, 20)
+        Me.TextBox5.TabIndex = 13
+        Me.TextBox5.Text = "sendMessage"
         '
-        'TextBox2
+        'Label5
         '
-        Me.TextBox2.Location = New System.Drawing.Point(9, 74)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(303, 20)
-        Me.TextBox2.TabIndex = 8
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 136)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(43, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Method"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(174, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Chat ID / Telegram Channel ID @ :"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(9, 113)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(303, 20)
+        Me.TextBox3.TabIndex = 10
         '
         'Label2
         '
@@ -204,21 +248,21 @@ Partial Class Form1
         Me.Label2.TabIndex = 9
         Me.Label2.Text = "Bot API Token :"
         '
-        'TextBox3
+        'TextBox2
         '
-        Me.TextBox3.Location = New System.Drawing.Point(9, 113)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(303, 20)
-        Me.TextBox3.TabIndex = 10
+        Me.TextBox2.Location = New System.Drawing.Point(9, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(303, 20)
+        Me.TextBox2.TabIndex = 8
         '
-        'Label3
+        'Label1
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 97)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(174, 13)
-        Me.Label3.TabIndex = 11
-        Me.Label3.Text = "Chat ID / Telegram Channel ID @ :"
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Interval :"
         '
         'GroupBox3
         '
@@ -234,14 +278,6 @@ Partial Class Form1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Sending"
         '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(132, 35)
-        Me.TextBox4.Multiline = True
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(165, 176)
-        Me.TextBox4.TabIndex = 8
-        '
         'Label4
         '
         Me.Label4.AutoSize = True
@@ -251,56 +287,120 @@ Partial Class Form1
         Me.Label4.TabIndex = 9
         Me.Label4.Text = "Caption if media :"
         '
-        'Label5
+        'TextBox4
         '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 136)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(43, 13)
-        Me.Label5.TabIndex = 12
-        Me.Label5.Text = "Method"
-        '
-        'TextBox5
-        '
-        Me.TextBox5.Location = New System.Drawing.Point(9, 152)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(302, 20)
-        Me.TextBox5.TabIndex = 13
-        Me.TextBox5.Text = "sendMessage"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(264, 169)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(40, 13)
-        Me.Label6.TabIndex = 10
-        Me.Label6.Text = "Total : "
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(41, 175)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(225, 13)
-        Me.Label7.TabIndex = 14
-        Me.Label7.Text = "Message / Audio / Document / Photo / Video"
+        Me.TextBox4.Location = New System.Drawing.Point(132, 35)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(165, 176)
+        Me.TextBox4.TabIndex = 8
         '
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(343, 243)
+        Me.PictureBox1.Location = New System.Drawing.Point(472, 219)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(439, 179)
+        Me.PictureBox1.Size = New System.Drawing.Size(316, 219)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 12
         Me.PictureBox1.TabStop = False
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label15)
+        Me.GroupBox4.Controls.Add(Me.Label14)
+        Me.GroupBox4.Controls.Add(Me.Label13)
+        Me.GroupBox4.Controls.Add(Me.Label12)
+        Me.GroupBox4.Controls.Add(Me.Label11)
+        Me.GroupBox4.Controls.Add(Me.Label10)
+        Me.GroupBox4.Controls.Add(Me.Label9)
+        Me.GroupBox4.Controls.Add(Me.Label8)
+        Me.GroupBox4.Controls.Add(Me.Label6)
+        Me.GroupBox4.Location = New System.Drawing.Point(325, 211)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(135, 227)
+        Me.GroupBox4.TabIndex = 13
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Info"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(6, 46)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(35, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Sent :"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(7, 66)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(64, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Remainder :"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(6, 85)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(46, 13)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "Status : "
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(7, 107)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(49, 13)
+        Me.Label11.TabIndex = 14
+        Me.Label11.Text = "Method :"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(7, 129)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(49, 13)
+        Me.Label12.TabIndex = 15
+        Me.Label12.Text = "Chat ID :"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(7, 151)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(54, 13)
+        Me.Label13.TabIndex = 16
+        Me.Label13.Text = "Success :"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(8, 173)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(38, 13)
+        Me.Label14.TabIndex = 17
+        Me.Label14.Text = "Error : "
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(8, 196)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(42, 13)
+        Me.Label15.TabIndex = 18
+        Me.Label15.Text = "Done : "
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
@@ -309,7 +409,7 @@ Partial Class Form1
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
-        Me.Text = "TGList Sender - Rasi Tech V 2.0 Beta"
+        Me.Text = "TGList Sender - Rasi Tech V3 .0"
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
@@ -318,6 +418,8 @@ Partial Class Form1
         Me.GroupBox3.ResumeLayout(False)
         Me.GroupBox3.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -348,4 +450,13 @@ Partial Class Form1
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
 End Class
--- TGList/Form1.vb
+++ TGList/Form1.vb
@@ -1,5 +1,11 @@
 ﻿Imports System.Net
 Public Class Form1
+
+    Dim sent As Integer
+    Dim done As Integer
+    Dim Errors As Integer
+    Dim rlist As Integer
+    Dim s As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             For X As Integer = 0 To TextBox1.Lines.Count - 1
@@ -7,6 +13,8 @@ Public Class Form1
             Next
             ListBox1.SelectedIndex = 0
             Label6.Text = "Total : " & ListBox1.Items.Count - 1
+            rlist = ListBox1.Items.Count - 1
+            Label9.Text = "Remainder : " & rlist
         Catch ex As Exception
 
         End Try
@@ -27,78 +35,158 @@ Public Class Form1
         Try
             If TextBox5.Text = "sendMessage" Then
                 Try
+                    Label10.Text = "Status : Sending..."
                     Dim IP As New WebClient
                     IP.DownloadString("https://api.telegram.org/bot" & TextBox2.Text & "/sendMessage?text=" & ListBox1.SelectedItem & "&chat_id=" & TextBox3.Text)
                     ListBox2.Items.Add("Success!")
+                    Label10.Text = "Status : Success!"
+                    Label12.Text = "Chat ID : " & TextBox3.Text
+                    Label11.Text = "Method : " & TextBox5.Text
+                    rlist -= 1
+                    Label9.Text = "Remainder : " & rlist
+                    s += 1
+                    Label13.Text = "Success : " & s
+                    sent += 1
+                    Label8.Text = "Sent : " & sent
                     ListBox1.SelectedIndex += 1
                     ListBox2.SelectedIndex += 1
                     If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
                         Timer1.Stop()
+                        done += 1
+                        Label15.Text = "Done : " & done
                         ListBox2.Items.Add("Done...")
+                        Label10.Text = "Status : Done..."
                     End If
                 Catch ex As Exception
                     Timer1.Stop()
+                    Errors += 1
+                    Label14.Text = "Error : " & Errors
                     ListBox2.Items.Add("Error!")
+                    Label10.Text = "Status : Error!"
                 End Try
             ElseIf TextBox5.Text = "sendPhoto" Then
                 Try
+                    Label10.Text = "Status : Sending..."
                     Dim IP As New WebClient
                     IP.DownloadString("https://api.telegram.org/bot" & TextBox2.Text & "/sendPhoto?photo=" & ListBox1.SelectedItem & "&chat_id=" & TextBox3.Text & "&caption=" & TextBox4.Text)
                     ListBox2.Items.Add("Success!")
+                    Label10.Text = "Status : Success!"
+                    Label11.Text = "Method : " & TextBox5.Text
+                    Label12.Text = "Chat ID : " & TextBox3.Text
+                    rlist -= 1
+                    Label9.Text = "Remainder : " & rlist
+                    s += 1
+                    Label13.Text = "Success : " & s
+                    sent += 1
+                    Label8.Text = "Sent : " & sent
                     ListBox1.SelectedIndex += 1
                     ListBox2.SelectedIndex += 1
                     If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
                         Timer1.Stop()
+                        done += 1
+                        Label15.Text = "Done : " & done
                         ListBox2.Items.Add("Done...")
+                        Label10.Text = "Status : Done..."
                     End If
                 Catch ex As Exception
                     Timer1.Stop()
+                    Errors += 1
+                    Label14.Text = "Error : " & Errors
                     ListBox2.Items.Add("Error!")
+                    Label10.Text = "Status : Error!"
                 End Try
             ElseIf TextBox5.Text = "sendAudio" Then
                 Try
+                    Label10.Text = "Status : Sending..."
                     Dim IP As New WebClient
                     IP.DownloadString("https://api.telegram.org/bot" & TextBox2.Text & "/sendAudio?audio=" & ListBox1.SelectedItem & "&chat_id=" & TextBox3.Text & "&caption=" & TextBox4.Text)
                     ListBox2.Items.Add("Success!")
+                    Label10.Text = "Status : Success!"
+                    Label11.Text = "Method : " & TextBox5.Text
+                    Label12.Text = "Chat ID : " & TextBox3.Text
+                    rlist -= 1
+                    Label9.Text = "Remainder : " & rlist
+                    s += 1
+                    Label13.Text = "Success : " & s
+                    sent += 1
+                    Label8.Text = "Sent : " & sent
                     ListBox1.SelectedIndex += 1
                     ListBox2.SelectedIndex += 1
                     If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
                         Timer1.Stop()
+                        done += 1
+                        Label15.Text = "Done : " & done
                         ListBox2.Items.Add("Done...")
+                        Label10.Text = "Status : Done..."
                     End If
                 Catch ex As Exception
                     Timer1.Stop()
+                    Errors += 1
+                    Label14.Text = "Error : " & Errors
                     ListBox2.Items.Add("Error!")
+                    Label10.Text = "Status : Error!"
                 End Try
             ElseIf TextBox5.Text = "sendDocument" Then
                 Try
+                    Label10.Text = "Status : Sending..."
                     Dim IP As New WebClient
                     IP.DownloadString("https://api.telegram.org/bot" & TextBox2.Text & "/sendDocument?document=" & ListBox1.SelectedItem & "&chat_id=" & TextBox3.Text & "&caption=" & TextBox4.Text)
                     ListBox2.Items.Add("Success!")
+                    Label10.Text = "Status : Success!"
+                    Label11.Text = "Method : " & TextBox5.Text
+                    Label12.Text = "Chat ID : " & TextBox3.Text
+                    rlist -= 1
+                    Label9.Text = "Remainder : " & rlist
+                    s += 1
+                    Label13.Text = "Success : " & s
+                    sent += 1
+                    Label8.Text = "Sent : " & sent
                     ListBox1.SelectedIndex += 1
                     ListBox2.SelectedIndex += 1
                     If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
                         Timer1.Stop()
+                        done += 1
+                        Label15.Text = "Done : " & done
                         ListBox2.Items.Add("Done...")
+                        Label10.Text = "Status : Done..."
                     End If
                 Catch ex As Exception
                     Timer1.Stop()
+                    Errors += 1
+                    Label14.Text = "Error : " & Errors
                     ListBox2.Items.Add("Error!")
+                    Label10.Text = "Status : Error!"
                 End Try
             ElseIf TextBox5.Text = "sendVideo" Then
                 Try
+                    Label10.Text = "Status : Sending..."
                     Dim IP As New WebClient
                     IP.DownloadString("https://api.telegram.org/bot" & TextBox2.Text & "/sendVideo?video=" & ListBox1.SelectedItem & "&chat_id=" & TextBox3.Text & "&caption=" & TextBox4.Text)
                     ListBox2.Items.Add("Success!")
+                    Label10.Text = "Status : Success!"
+                    Label11.Text = "Method : " & TextBox5.Text
+                    Label12.Text = "Chat ID : " & TextBox3.Text
+                    rlist -= 1
+                    Label9.Text = "Remainder : " & rlist
+                    s += 1
+                    Label13.Text = "Success : " & s
+                    sent += 1
+                    Label8.Text = "Sent : " & sent
                     ListBox1.SelectedIndex += 1
                     ListBox2.SelectedIndex += 1
                     If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
                         Timer1.Stop()
+                        done += 1
+                        Label15.Text = "Done : " & done
                         ListBox2.Items.Add("Done...")
+                        Label10.Text = "Status : Done..."
                     End If
                 Catch ex As Exception
                     Timer1.Stop()
+                    Errors += 1
+                    Label14.Text = "Error : " & Errors
                     ListBox2.Items.Add("Error!")
+                    Label10.Text = "Status : Error!"
                 End Try
             End If
 
